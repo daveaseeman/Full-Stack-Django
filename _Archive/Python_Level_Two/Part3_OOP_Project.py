8a2ddf99f70e5868ad33750b8da6df4051994b94
Dave A Seeman <daveseeman@icloud.com>
2017-11-28 13:01:54 -0600

    initial commit

---
+++ _Archive/Python_Level_Two/Part3_OOP_Project.py
@@ -0,0 +1,115 @@
+#####################################
+### WELCOME TO YOUR OOP PROJECT #####
+#####################################
+
+# For this project you will be using OOP to create a card game. This card game will
+# be the card game "War" for two players, you an the computer. If you don't know
+# how to play "War" here are the basic rules:
+#
+# The deck is divided evenly, with each player receiving 26 cards, dealt one at a time,
+# face down. Anyone may deal first. Each player places his stack of cards face down,
+# in front of him.
+#
+# The Play:
+#
+# Each player turns up a card at the same time and the player with the higher card
+# takes both cards and puts them, face down, on the bottom of his stack.
+#
+# If the cards are the same rank, it is War. Each player turns up three cards face
+# down and one card face up. The player with the higher cards takes both piles
+# (six cards). If the turned-up cards are again the same rank, each player places
+# another card face down and turns another card face up. The player with the
+# higher card takes all 10 cards, and so on.
+#
+# There are some more variations on this but we will keep it simple for now.
+# Ignore "double" wars
+#
+# https://en.wikipedia.org/wiki/War_(card_game)
+
+from random import shuffle
+
+# Two useful variables for creating Cards.
+SUITE = 'H D S C'.split()
+RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+
+# mycards = [(s,r) for s SUITE for r in RANKS]
+# same as:
+# for r in RANKS:
+#     for s in SUITE:
+#         mycards.append((s,r))
+
+
+class Deck:
+    """
+    This is the Deck Class. This object will create a deck of cards to initiate
+    play. You can then use this Deck list of cards to split in half and give to
+    the players. It will use SUITE and RANKS to create the deck. It should also
+    have a method for splitting/cutting the deck in half and Shuffling the deck.
+    """
+
+    def __init__(self):
+        """ List comprehension (instead of nested for loop) of suite and rank """
+        print("Creating new ordered deck")
+        self.allcards = [(s, r) for s SUITE for r in RANKS]
+
+    def shuffle(self):
+        """ Calls shuffle function"""
+        print("Shuffling deck")
+        shuffle(self.allcards)
+
+    def split_in_half(self):
+        """ Tuple of split cards """
+        return (self.allcards[:26], self.allcards(26:]))
+
+
+class Hand:
+    '''
+    This is the Hand class. Each player has a Hand, and can add or remove
+    cards from that hand. There should be an add and remove card method here.
+    '''
+    def __init__(self, cards):
+        self.cards= cards
+
+    def __str__(self):
+        """ Return number of cards """
+        return "Contains {} cards".format(len(self.cards))
+
+    def add(self, added_cards):
+        """ Extend the hand with the added_cards list """
+        self.cards.extend(added_cards)
+
+    def remove_card(self):
+        """ Remove the last card """
+        return self.cards.pop()
+
+class Player:
+    """
+    This is the Player class, which takes in a name and an instance of a Hand
+    class object. The Payer can then play cards and check if they still have cards.
+    """
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
+
+    def play_card(self):
+        drawn_card = self.hand.remove_card()
+        print("{} has placed: {}\n".format(self.name, drawn_card))
+        return drawn_card
+
+    def remove_ward_cards(self):
+        war_cards = []
+        for x in range(3):
+            war_cards.append(self.hand.remove_card())
+        return war_cards
+
+    def still_has_cards(self):
+        """ Return true if player still has cards """
+        return len(self.hand.cards != 0)
+
+
+######################
+#### GAME PLAY #######
+######################
+print("Welcome to War, let's begin...")
+
+# Use the 3 classes along with some logic to play a game of war!
